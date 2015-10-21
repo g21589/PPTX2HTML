0843071d2194f2b37f1cf66d7d6ffa98c2fe0ea1
g21589 <g21589@gmail.com>
2015-10-21 20:58:17 +0800

    Support display jpg and png image

--- js/pptx2html.js
+++ js/pptx2html.js
@@ -137,6 +137,42 @@ function getSlideSize(zip) {
 	};
 }
 
+function base64ArrayBuffer(arrayBuffer) {
+	var base64    = '';
+	var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+	var bytes         = new Uint8Array(arrayBuffer);
+	var byteLength    = bytes.byteLength;
+	var byteRemainder = byteLength % 3;
+	var mainLength    = byteLength - byteRemainder;
+
+	var a, b, c, d;
+	var chunk;
+
+	for (var i = 0; i < mainLength; i = i + 3) {
+		chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2];
+		a = (chunk & 16515072) >> 18;
+		b = (chunk & 258048)   >> 12;
+		c = (chunk & 4032)     >>  6;
+		d = chunk & 63;
+		base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d];
+	}
+
+	if (byteRemainder == 1) {
+		chunk = bytes[mainLength];
+		a = (chunk & 252) >> 2;
+		b = (chunk & 3)   << 4;
+		base64 += encodings[a] + encodings[b] + '==';
+	} else if (byteRemainder == 2) {
+		chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1];
+		a = (chunk & 64512) >> 10;
+		b = (chunk & 1008)  >>  4;
+		c = (chunk & 15)    <<  2;
+		base64 += encodings[a] + encodings[b] + encodings[c] + '=';
+	}
+
+	return base64;
+}
+
 (function () {
 
 	if (!window.FileReader || !window.ArrayBuffer) {
@@ -267,6 +303,16 @@ function getSlideSize(zip) {
 									text += "</div>";
 								//}
 								context += text;
+							});
+							
+							$slideXML.find("pic").each(function(index, node) {
+								var $node = $(node);
+								var rid = $node.find("blip").attr("r:embed");
+								var $xfrmNode = $node.find("spPr").find("xfrm");
+								var imgName = openXMLFromZip(zip, resName).find("Relationship[Id=\"" + rid + "\"]").attr("Target").replace("../", "ppt/");
+								var imgArrayBuffer = zip.file(imgName).asArrayBuffer();
+								context += "<div class='block content' style='" + getPosition($xfrmNode, null, null) + getSize($xfrmNode, null, null) + 
+										   "'><img src=\"data:image/jpeg;base64," + base64ArrayBuffer(imgArrayBuffer) + "\" style='width: 100%; height: 100%'/></div>";
 							});
 							
 							$fileContent.append($("<li>", {
