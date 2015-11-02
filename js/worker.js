9224d795917d49b65c8c44514a210b1921e10bb7
g21589 <g21589@gmail.com>
2015-11-02 17:31:58 +0800

    Temp

--- js/worker.js
+++ js/worker.js
@@ -4,7 +4,7 @@ importScripts(
 	'./colz.class.min.js',
 	'./highlight.min.js',
 	'./functions.js',
-	'./tXmlUnfolded.min.js'
+	'./tXmlUnfolded.js'
 );
 
 onmessage = function(e) {
@@ -16,12 +16,33 @@ onmessage = function(e) {
 		"data": base64ArrayBuffer(zip.file("docProps/thumbnail.jpeg").asArrayBuffer())
 	});
 	
-	var jsonContent = tXml(zip.file("ppt/presentation.xml").asText(), { simplify: 1 });
-	//var jsonContent = xmlToJSON.parseString( "", {xmlns: false}/*zip.file("ppt/presentation.xml").asText()*/ );
+	var ContentTypesJson = readXmlFile(zip, "[Content_Types].xml");
+	var subObj = ContentTypesJson["?xml"]["Types"]["Override"];
+	for (var i=0; i<subObj.length; i++) {
+		switch (subObj[i]["attrs"]["ContentType"]) {
+			case "application/vnd.openxmlformats-officedocument.presentationml.slide+xml":
+				self.postMessage({
+					"type": "INFO",
+					"data": subObj[i]["attrs"]["PartName"]
+				});
+				break;
+			default:
+		}
+		
+	}
+	//self.postMessage({
+	//	"type": "INFO",
+	//	"data": JSON.stringify( ContentTypes )
+	//});
 	
-	self.postMessage({
-		"type": "INFO",
-		"data": JSON.stringify(jsonContent)
-	});
+	var presentationJson = readXmlFile(zip, "ppt/presentation.xml");
+	//self.postMessage({
+	//	"type": "INFO",
+	//	"data": JSON.stringify( presentationJson )
+	//});
 	
+}
+
+function readXmlFile(zip, filename) {
+	return tXml(zip.file(filename).asText());
 }
